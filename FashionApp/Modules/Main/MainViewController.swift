982f8b5ce4cde65e525d316068c8a5435e92ac25
Andrew Zhegalik <andrew.zhegalik@gmail.com>
2023-08-04 20:55:19 +0200

    dfdfdfd

--- FashionApp/Modules/Main/MainViewController.swift
+++ FashionApp/Modules/Main/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        print("Hello")
     }
 }
 
