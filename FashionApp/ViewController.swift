f83e54f7b84debd36e2726bde100d5b7f5e4c0e2
esifyce <49156359+esifyce@users.noreply.github.com>
2023-08-04 21:52:11 +0600

    Initial Commit

---
+++ FashionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FashionApp
+//
+//  Created by Krasivo on 04.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
