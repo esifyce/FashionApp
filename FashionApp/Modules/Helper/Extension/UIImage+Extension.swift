f9ae42270401daf83da04a1fda69dbcabaca5b52
Andrew Zhegalik <andrew.zhegalik@gmail.com>
2023-08-08 12:53:09 +0200

    paywall

--- FashionApp/Modules/Helper/Extension/UIImage+Extension.swift
+++ FashionApp/Modules/Helper/Extension/UIImage+Extension.swift
@@ -12,10 +12,89 @@ extension UIImage {
         public static var infoIcon: UIImage = .init(named: "infoBar")!
         public static var settingIcon: UIImage = .init(named: "settingBar")!
     }
+    
+    enum PayWall {
+        public static var closeIcon: UIImage = .init(named: "close")!
+        public static var logo: UIImage = .init(named: "payWallLogo")!
+    }
 }
 
 extension CGFloat {
     var autoIpadHeightSize: CGFloat { return UIScreen.main.bounds.height * self / 1194 }
     var autoIpadHeightLandscapeSize: CGFloat { return UIScreen.main.bounds.width * self / 1194 }
     var autoIphoneHeightSize: CGFloat { return UIScreen.main.bounds.height * self / 844.0 }
+}
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}
+
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+    
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: font], context: nil)
+
+        return ceil(boundingBox.width)
+    }
+}
+
+
+extension Array where Element: UIView {
+    func toStackView(axis: NSLayoutConstraint.Axis = .vertical, spacing: CGFloat = 0, distibution: UIStackView.Distribution = .fillEqually, alignment: UIStackView.Alignment = .leading) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.axis = axis
+        stackView.alignment = alignment
+        stackView.distribution = distibution
+        stackView.spacing = spacing
+        for element in self {
+            stackView.addArrangedSubview(element)
+        }
+        
+        return stackView
+    }
+}
+
+extension CALayer {
+  func applyFigmaShadow(
+    color: UIColor = .black,
+    alpha: Float = 0.5,
+    x: CGFloat = 0,
+    y: CGFloat = 2,
+    blur: CGFloat = 4,
+    spread: CGFloat = 0)
+  {
+    masksToBounds = true
+    shadowColor = color.cgColor
+    shadowOpacity = alpha
+    shadowOffset = CGSize(width: x, height: y)
+    shadowRadius = blur / 2.0
+    if spread == 0 {
+      shadowPath = nil
+    } else {
+      let dx = -spread
+      let rect = bounds.insetBy(dx: dx, dy: dx)
+      shadowPath = UIBezierPath(rect: rect).cgPath
+    }
+  }
 }
---
+++ FashionApp/Modules/Paywall/View/Views/TrialView.swift
@@ -0,0 +1,89 @@
+//
+//  TrialView.swift
+//  FashionApp
+//
+//  Created by Andrey on 8.08.23.
+//
+
+import UIKit
+import FrameBuilder
+
+
+final class TrialView: UIView {
+    var startCallBack: (() -> ())?
+    var seeOtherCallBack: (() -> ())?
+    
+    private lazy var startButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Start 3-Day Free Trial", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(rgb: 0x1777F0)
+        button.layer.cornerRadius = 16
+        button.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var seeOtherButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("See other plan", for: .normal)
+        button.setTitleColor(UIColor(rgb: 0xCACBD0), for: .normal)
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(seeOtherButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupUI()
+    }
+}
+
+private extension TrialView {
+    func setupUI() {
+        addSubviews()
+        makeLayout()
+        
+        layer.applyFigmaShadow(color: .red, alpha: 1, x: 0, y: -8, blur: 32, spread: 0)
+    }
+    
+    func addSubviews() {
+        //[seeOtherButton].forEach({ addSubview($0) })
+    }
+    
+    func makeLayout() {
+        startButton.buildFrame(
+            FrameBuilder()
+                .x(24)
+                .top(equalTo: .top, ofView: self, withOffset: 16)
+                .width(bounds.size.width - 48)
+                .height(56)
+        )
+
+        seeOtherButton.buildFrame(
+            FrameBuilder()
+                .top(equalTo: .bottom, ofView: startButton, withOffset: 24)
+                .centerXToCenterX(ofView: self, offset: -50)
+                .width(100)
+                .height(24)
+        )
+    }
+}
+
+@objc
+private extension TrialView {
+    func startButtonTapped() {
+        startCallBack?()
+    }
+    
+    func seeOtherButtonTapped() {
+        seeOtherCallBack?()
+    }
+}
